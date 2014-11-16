80cec09dce11d35b45fcee88140ceb3f1b137b59
Antony Korah <antonykorah@gmail.com>
2014-11-15 21:49:36 -0500

    storing data

--- signup.js
+++ signup.js
@@ -1,25 +1,39 @@
-<!--
+
 
 var firebaseUsers = new Firebase("https://blinding-inferno-7166.firebaseio.com/users");
 var firebaseSkills = new Firebase("https://blinding-inferno-7166.firebaseio.com/skills");
 
-var email = document.getElementById("inputEmail");  
-var password = document.getElementById("inputPassword");
-var skills = document.getElementById("inputSkills");
 
-document.getElementById("submitBtn").addEventListener("click", addUser);
 
-function addUser()
+// document.getElementById("submitBtn").unbind("click");
+document.getElementById("submitBtn").addEventListener("click",function()
 {
+	var email = document.getElementById("inputEmail").value;  
+	var password = document.getElementById("inputPassword").value;
+	var skills = document.getElementById("inputCourses").value;
+
 	firebaseUsers.set(
-		{ 	user1:{
-			email: "werty@duke.edu",
-			password: "enter123"
+		{ "email":{
+			password: password
 		}
 	});
-}
+
+	console.log("Clicky");
+
+}, false);
+
+// document.getElementById("submitBtn").addEventListener("click", addUser);
+
+// function addUser()
+// {
+// 	firebaseUsers.set(
+// 		{ 	user1:{
+// 			email: "werty@duke.edu",
+// 			password: "enter123"
+// 		}
+// 	});
+// }
 
 
 
 
--->
